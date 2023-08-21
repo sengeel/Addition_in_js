0580cfb57649aafe1e3715229cfcaec2cefd932b
sengeel <okereolujiechizurum@gmail.com>
2023-08-21 20:50:49 +0100

    addition in javascript

---
+++ script.js
@@ -0,0 +1,101 @@
+// const firstname = 'Chizurum';
+// console.log(firstname);
+
+// const age = '19';
+
+// const age2 = 19;
+
+// const num3 = 85
+
+// const num1 = 98;
+// console.log(num1);
+
+// const num2 = 65;
+// console.log(num2);
+
+
+
+// const add = (num1 + num2) / num3;
+// console.log(add);
+
+// const pi = Math.PI;
+// const r = 89;
+
+// const area = pi * r**2
+// console.log(area);
+
+// const num1 = 19
+
+// const num2 = 20
+
+// const area = (num1 * num2) / 2
+// console.log(area)
+
+// AREA OF A PENTAGON
+// const a = 8;
+
+// const sqrt1 = Math.sqrt(5)
+
+// const sqrt2 = Math.sqrt(5 *(5 + 2 * sqrt1));
+
+// const area = 1/4 * sqrt2 * a**2
+// console.log(area)
+
+// AREA OF A TRAPEZIOD
+// const a = 7
+
+// const b = 5
+
+// const h = 3
+
+// const area = (a + b) /2 *h
+// console.log(area)
+
+// AREA OF A DECAGON
+// const a = 4;
+
+// const sqrt1 = Math.sqrt(5)
+
+// const sqrt2 = Math.sqrt(5 + 2 * sqrt1)
+
+// const sqrt3 = a**2 * sqrt2
+
+// const area = (5/2) * sqrt3
+// console.log(area)
+
+// AREA OF A HEXAGON
+
+// const a = 3;
+
+// const sqrt1 = Math.sqrt(3);
+// const sqrt2 = (3 * sqrt1) / 2
+
+// const area = sqrt2 * a**2
+// console.log(area)
+
+// VOLUME OF RIGHT RECTANGULAR PYRAMID
+// const area = (3 * 4 * 5) / 3
+// console.log(area)
+
+// AREA OF A RIGHT RECTANGULAR PYRAMID ASSIGNMENT
+
+const l = 3
+
+const w = 6
+
+const h = 4
+
+const sqrt1 = Math.sqrt((w/2)**2 + h**2)
+
+const sqrt2 = l * sqrt1
+
+const sqrt3 = (l * w) + sqrt2
+
+const sqrt4 = Math.sqrt((l / 2)**2 + h**2)
+
+const sqrt5 = w * sqrt4
+
+const sqrt6 = sqrt3 + sqrt5
+console.log(sqrt6)
+
+
